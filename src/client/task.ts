47519b660de03acc7b4d9b9e123ad99d48e752d8
iAmir <hhm6@yahoo.com>
2023-01-08 12:32:02 +0330

    wrap pawn args around " "

--- src/client/task.ts
+++ src/client/task.ts
@@ -11,9 +11,9 @@ export const task = `{
 			"args": [
 				"\${relativeFile}",
 				"-Dgamemodes",
-				"-;+",
-				"-(+",
-				"-d3"
+				"\"-;+\"",
+				"\"-(+\"",
+				"\"-d3\""
 			],
 			"group": {
 				"kind": "build",
