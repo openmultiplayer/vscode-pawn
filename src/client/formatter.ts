ac57e236ba7779926d9dcdb7df76826030d85acf
iAmir <hhm6@yahoo.com>
2023-01-13 14:24:42 +0330

    fix many issues with formatting, and use new backend

--- src/client/formatter.ts
+++ src/client/formatter.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import * as jsbeautifier from "js-beautify";
+import { format } from "astyle";
 
 interface RegexCodeFix {
   expr: RegExp;
@@ -8,43 +8,64 @@ interface RegexCodeFix {
 
 const beforeFix: RegexCodeFix[] = [
   { expr: /(^[ \t]+#|^#)/gm, replacement: "//pawnd_tag_hash_$1" },
+  { expr: /case\s*(\S*)\s*:\s*(\w+\s*.*;)/gm, replacement: "case $1pawnd_switch_case_signle_line$2" },
   { expr: /([^\s:]):([^\s:])(?=(?:[^"]*"[^"]*")*[^"]*$)/gm, replacement: "$1pawnd_tag_semicolon$2" },
   { expr: /([^\s:])::([^\s:])(?=(?:[^"]*"[^"]*")*[^"]*$)/gm, replacement: "$1pawnd_tag_two_semicolon$2" },
-  { expr: /([^\s:]):: +([^\s:])(?=(?:[^"]*"[^"]*")*[^"]*$)/gm, replacement: "$1pawnd_tag_four_semicolon$2" },
-  { expr: /([\w^\s:]): +([^\s:])(?=(?:[^"]*"[^"]*")*[^"]*$)/gm, replacement: "$1pawnd_tag_three_semicolon$2" },
   { expr: /([^\s:])@([^\s:])(?=(?:[^"]*"[^"]*")*[^"]*$)/gm, replacement: "$1pawnd_tag_at$2" },
   { expr: /\bconst\b/gm, replacement: "pawnd_tag_const" },
 ];
 
 const afterFix: RegexCodeFix[] = [
   { expr: /\bpawnd_tag_const\b/gm, replacement: "const" },
+  { expr: /case(.*)pawnd_switch_case_signle_line/gm, replacement: "case$1: " },
   { expr: /pawnd_tag_semicolon/gm, replacement: ":" },
   { expr: /pawnd_tag_two_semicolon/gm, replacement: "::" },
-  { expr: /pawnd_tag_three_semicolon/gm, replacement: ": " },
-  { expr: /pawnd_tag_four_semicolon/gm, replacement: ":: " },
   { expr: /pawnd_tag_at/gm, replacement: "@" },
-  // { expr: /(^if +\(|^[ \t]+if +\()(?=(?:[^"]*"[^"]*")*[^"]*$)/gm, replacement: "if(" },
   { expr: />(\s+)\nhook/gm, replacement: ">\nhook" },
   { expr: /static(\s+)const/gm, replacement: "static const" },
   { expr: /\.\s\./gm, replacement: ".." },
   { expr: /^[ \t]+\/\/pawnd_tag_hash_|^\/\/pawnd_tag_hash_/gm, replacement: "" },
+  { expr: /CMD(.*):\r\n(.*)\(/gmi, replacement: "CMD$1:$2(" },
+  { expr: /CMD(.*):\n(.*)\(/gmi, replacement: "CMD$1:$2(" }
 ];
 
-const formatPawn = (content: string) => {
+const formatPawn = async (content: string) => {
   const brace_style = vscode.workspace.getConfiguration().get("pawn.language.brace_style") as
-    | "collapse"
-    | "expand"
-    | "end-expand"
-    | "none"
-    | "preserve-inline"
+    | "Allman"
+    | "K&R"
+    | "Stroustrup"
     | null;
+
   for (const key in beforeFix) {
     const element = beforeFix[key];
     content = content.replace(element.expr, element.replacement);
   }
-  content = jsbeautifier.js_beautify(content, {
-    brace_style: brace_style === null ? undefined : brace_style,
-  });
+
+  const style = (() => {
+    if (brace_style === "Allman")
+      return "allman"
+    else if (brace_style === "K&R")
+      return "kr"
+    else if (brace_style === "Stroustrup")
+      return "stroustrup"
+    else
+      return "allman"
+  })
+
+  const formatterConfig = [
+    `--style=${style()}`,
+    "--indent-switches",
+    "--indent-preproc-define",
+    "--indent-col1-comments",
+    "--indent-preproc-block",
+    "--pad-comma",
+    "--pad-oper",
+    "--unpad-paren",
+    "--pad-header",
+    "--attach-return-type"
+  ]
+
+  content = await format(content, formatterConfig.join(" "));
   for (const key in afterFix) {
     const element = afterFix[key];
     content = content.replace(element.expr, element.replacement);
@@ -53,18 +74,18 @@ const formatPawn = (content: string) => {
 };
 
 const PawnDocumentFormattingEditProvider = {
-  provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.ProviderResult<Array<vscode.TextEdit>> {
+  async provideDocumentFormattingEdits(document: vscode.TextDocument) {
     const edits: vscode.TextEdit[] = [];
     let content = document.getText();
-    content = formatPawn(content);
+    content = await formatPawn(content);
     const range = new vscode.Range(new vscode.Position(0, 0), document.lineAt(document.lineCount - 1).range.end);
     edits.push(new vscode.TextEdit(range, content));
     return edits;
   },
-  provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range): vscode.ProviderResult<Array<vscode.TextEdit>> {
+  async provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range) {
     const edits: vscode.TextEdit[] = [];
     let content = document.getText(range);
-    content = formatPawn(content);
+    content = await formatPawn(content);
     edits.push(new vscode.TextEdit(range, content));
     return edits;
   },
